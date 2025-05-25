ceaf20389b39aa7eea8734b4fbeaae43b989bfed
hicham benaissa <hichambenaissa@MacBook-Pro.lan>
2025-05-25 20:01:35 +0200

    La2

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCroecK8YNkOg_I9rbORCmwXtbY9ngikBA",
   authDomain: "foot-ai.firebaseapp.com",
@@ -12,5 +16,7 @@ const firebaseConfig = {
   measurementId: "G-LVEH9QENN1"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 const analytics = getAnalytics(app);
